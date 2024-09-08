985fc8276363a048be7ebf021790b24dccce4218
Diya Sundeep <diyasundeep64@gmail.com>
2024-09-08 10:48:22 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-const start = document.getElementById('start')
-const page = document.getElementById('page')
-const bgImage = document.getElementById('prof');
+const start = document.getElementById("start");
+const page = document.getElementById("page");
+const bgImage = document.getElementById("prof");
 
+start.addEventListener("click", showPage, false);
 
-start.addEventListener('click',showPage,false)
-
-function showPage()
-{
-    
-    start.style.display = "none";
-    page.style.display = "block";
-    window.alert("This is my portfolio");
-    
+function showPage() {
+  page.style.display = "block";
+  window.alert("Hey Guys!");
 }
 
-bgImage.addEventListener('mouseover',()=> {
-    document.body.style.backgroundImage="url('./flowers.png')";
-    console.log("Hello World")
-})
+bgImage.addEventListener("mouseover", () => {
+  document.body.style.backgroundImage = "url('./img2.jpg')";
+  console.log("Hello World");
+});
 
-bgImage.addEventListener('mouseout', () => {
-    document.body.style.backgroundImage = ""; 
-    console.log(" NO Hello World")   
+bgImage.addEventListener("mouseout", () => {
+  document.body.style.backgroundImage = "";
+  console.log(" NO Hello World");
 });
